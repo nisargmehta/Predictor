ef57da4f0ea8ca73f69c792da9f303fd70c2e47c
nisargmehta <NisargMehta@Nisargs-Mac.local>
2019-06-30 18:00:35 -0700

    first commit

---
+++ Predictor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Predictor
+//
+//  Created by Nisarg Mehta on 6/30/19.
+//  Copyright © 2019 Open Source. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
